9819858cb7e416f307e06753befc794a18427ca0
chrisg522 <iamemmachris@gmail.com>
2025-02-25 20:43:29 -0800

    Resolved merge conflicts

---
+++ website/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Record(models.Model):
+    created_at =models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
