d493db2b2c1e3f8e81e4925087f3b47118044e7a
chrisg522 <iamemmachris@gmail.com>
2025-02-25 21:15:54 -0800

    Committing local changes before pull

--- dcrmproject/website/models.py
+++ dcrmproject/website/models.py
@@ -4,4 +4,5 @@ class Record(models.Model):
     created_at =models.DateTimeField(auto_now_add=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.EmailField(unique=True)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
